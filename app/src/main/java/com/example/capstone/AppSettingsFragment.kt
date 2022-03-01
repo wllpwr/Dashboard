ab166dd6e2b82d5f0534fe074ce63936fee836a2
Mike <michael.bamford@mymail.champlain.edu>
2022-03-01 17:26:22 -0500

    changed app settings page to use preference library/updated some dependencies

--- app/src/main/java/com/example/capstone/AppSettingsFragment.kt
+++ app/src/main/java/com/example/capstone/AppSettingsFragment.kt
@@ -1,18 +1,13 @@
 package com.example.capstone
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
 
 
-class AppSettingsFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_app_settings, container, false)
+class AppSettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.app_settings, rootKey)
     }
+
+
 }
